272a56cd4148a8d81427e6cde7054ab98a47a60e
ChieChieBun <104299226+ChieChieBun@users.noreply.github.com>
2022-06-24 18:54:04 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var player1= Math.floor((Math.random()*6)+1)
+var player2= Math.floor((Math.random()*6)+1)
+
+// Image Change
+
+var player1img= ("images/dice" + player1+ ".png")
+var player2img= ("images/dice" + player2+ ".png")
+document.querySelector(".img1").setAttribute("src", player1img);
+document.querySelector(".img2").setAttribute("src", player2img);
+
+// Word change
+ 
+if (player1>player2){
+  document.querySelector("h1").innerHTML= "🚩 Player 1 Wins!";
+}
+if (player2>player1){
+  document.querySelector("h1").innerHTML= " Player 2 Wins!🚩";
+}
+if (player1===player2){
+  document.querySelector("h1").innerHTML= "🚩 DRAW🚩";
+}
+
